d26992a30ba75923d5751f6fc949d4509d7a7437
niquito <ncaffarini@gmail.com>
2020-09-22 20:48:18 -0300

    Update first-post.js

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,3 +1,14 @@
+import Link from 'next/Link'
+
 export default function FirstPost() {
-  return <h1>First Post</h1>
+	return (
+		<>
+			<h1>First post</h1>
+			<h2>
+				<Link href="/">
+					<a>Volver</a>
+				</Link>
+			</h2>
+		</>
+	)
 }
